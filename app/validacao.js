22a54011ac940f956aa92a3fbb7d8776e9ac6bcf
Bruna Gomes <115025421+gcbruna@users.noreply.github.com>
2023-05-11 18:04:10 -0300

    Add files via upload

---
+++ app/validacao.js
@@ -0,0 +1,68 @@
+function  verificaSeOChutePossuiUmValorValido(chute) {
+    const numero = +chute
+   
+    if (chuteForInvalido(numero)) {
+        if (chute.toUpperCase() === "GAME OVER") {
+    
+            document.body.innerHTML =
+                `
+                <h2>Game Over!!!</h2>
+                <h3>Pressione o botão para jogar novamente</h3>
+                <button id="jogar-novamente" class="btn-jogar" >Jogar novamente</button>
+                <h4>O número secreto era ${numeroSecreto}</h4>
+                `
+                document.body.style.backgroundColor = "black";
+
+        } else {
+            elementoChute.innerHTML += '<div>Valor Inválido</div>';
+        }
+    }
+
+    if (chuteForInvalido(numero)) {
+        elementoChute.innerHTML += '<div>Valor inválido</div>'
+        return
+    }
+
+    if (numeroForMaiorOuMenorQueOValorPermitido(numero)) {
+        elementoChute.innerHTML += `
+            <div>Valor inválido: Fale um número 
+            entre ${menorValor} e ${maiorValor}</div>
+        `
+        return
+    }
+
+    if (numero === numeroSecreto) {
+        document.body.innerHTML = `
+            <h2>Você acertou!</h2>
+            <h3>O número secreto era ${numeroSecreto}</h3>
+
+            <button id="jogar-novamente" class="btn-jogar">Jogar novamente</button>
+        `
+    } 
+    
+    else if (numero > numeroSecreto) {
+        elementoChute.innerHTML += `
+            <div>O número secreto é menor <i class="fa-solid fa-arrow-down"></i></div>
+        `
+    } else {
+        elementoChute.innerHTML += `
+            <div>O número secreto é maior <i class="fa-solid fa-arrow-up"></i></div>
+        `
+    } 
+
+}
+
+function chuteForInvalido(numero) {
+    return Number.isNaN(numero)
+}
+
+function numeroForMaiorOuMenorQueOValorPermitido(numero) {
+    return numero > maiorValor || numero < menorValor
+}
+
+
+document.body.addEventListener('click', e => {
+    if(e.target.id == 'jogar-novamente') {
+        window.location.reload()
+    }
+})
